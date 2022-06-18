fd28f6ac8aca9ed3cd9d8da96c48fe4139aca3cc
Ricardo Carvalho <rat.carvalho@gmail.com>
2022-06-18 17:47:38 -0300

    dapui uses the default configuration now

--- dot_config/nvim/lua/dapconfig.lua
+++ dot_config/nvim/lua/dapconfig.lua
@@ -41,41 +41,4 @@ dap.configurations.python = {
   }
 }
 
-dapui.setup({
-  icons = { expanded = '▾', collapsed = '▸' },
-  mappings = {
-    expand = { '<CR>', '<2-LeftMouse>' },
-    open = 'o',
-    remove = 'd',
-    edit = 'e',
-    repl = 'r',
-  },
-  layouts = {
-    {
-      elements = {
-        {
-          id = 'scopes',
-          size = 0.25,
-        },
-        { id = 'breakpoints', size = 0.25 },
-        { id = 'stacks', size = 0.25 },
-        { id = 'watches', size = 00.25 },
-      },
-      size = 40,
-      position = 'left',
-    },
-    {
-      elements = { 'repl' },
-      size = 10,
-      position = 'bottom',
-    }
-  },
-  floating = {
-    max_height = nil,
-    max_width = nil,
-    mappings = {
-      close = { 'q', '<Esc>' },
-    },
-  },
-  windows = { indent = 1 },
-})
+dapui.setup({})

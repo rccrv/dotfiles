28779006a8a477f51428f8b9a1a8a29cce0194db
Ricardo Carvalho <rat.carvalho@gmail.com>
2023-02-12 19:38:30 -0300

    nvim: LSP configuration change

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -24,7 +24,7 @@ end
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
   settings = {

82342f32f11704aa73f4cb22c66005f7a2c15c42
Ricardo Carvalho <rat.carvalho@gmail.com>
2023-05-09 08:40:24 -0300

    migrated to wezterm. changed kitty configuration

---
+++ dot_wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Dark Pastel'
+config.enable_tab_bar = false
+config.window_background_opacity = 0.75
+config.initial_rows = 34
+config.initial_cols = 120
+
+config.font = wezterm.font 'DroidSansMono NF'
+config.font_size = 12.0
+
+config.audible_bell = "Disabled"
+
+return config
+

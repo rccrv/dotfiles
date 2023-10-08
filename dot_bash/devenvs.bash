d8323b640b0021651cef496330addf537d995472
Ricardo Carvalho <rat.carvalho@gmail.com>
2023-10-08 10:43:31 -0300

    bash: include configs for pnpm and pipx

--- dot_bash/devenvs.bash
+++ dot_bash/devenvs.bash
@@ -6,13 +6,9 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
 export PYTHONSTARTUP=$HOME/.pythonrc
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export PATH="$PYENV_ROOT/shims:$PATH"
+export PATH="$HOME/.userland/software/python3/bin:$PATH"
+python3 -m pipx ensurepath 1> /dev/null 2>&1
 export PATH="$HOME/.local/bin:$PATH"
-#if command -v pyenv 1>/dev/null 2>&1; then
-#  eval "$(pyenv init --path)"
-#fi
 
 # Rust #
 . "$HOME/.cargo/env"
@@ -20,6 +16,8 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 # Node #
 eval "$(fnm env)"
+export PNPM_HOME="/home/ricardo/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
 
 # Julia #
 export PATH="$HOME/.userland/software/julia/bin:$PATH"

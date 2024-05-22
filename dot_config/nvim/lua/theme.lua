79d32df59fe6f03c9127e81fc75309e21f54956e
Ricardo Carvalho <rat.carvalho@gmail.com>
2024-05-22 14:43:20 -0300

    nvim, foot, labwc: change configurations
    - nvim: reconfigure indent-blankline.nvim
    
    - foot: configure term as xterm-256color
    
    - labwc: reconfigure foot as default terminal

--- dot_config/nvim/lua/theme.lua
+++ dot_config/nvim/lua/theme.lua
@@ -11,10 +11,6 @@ vim.cmd [[highlight BufferTabpageFill guibg=none]]
 -- Whitespaces from listchar are have a red background
 vim.cmd [[highlight Whitespace guifg=red]]
 
--- Creates a non formated blankline
-local ibl = require 'ibl'
-ibl.setup({})
-
 -- Transparent theme
 local transparent = require 'transparent'
 transparent.setup({
@@ -22,6 +18,32 @@ transparent.setup({
     'NvimTreeNormal',
   }
 })
+
+-- Creates a non formated blankline
+local ibl = require 'ibl'
+local hooks = require 'ibl.hooks'
+
+local highlight = {
+  'RainbowRed',
+  'RainbowYellow',
+  'RainbowBlue',
+  'RainbowOrange',
+  'RainbowGreen',
+  'RainbowViolet',
+  'RainbowCyan',
+}
+
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+  vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+  vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+  vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+  vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+  vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+  vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+end)
+
+ibl.setup { indent = { highlight = highlight } }
 
 rainbow_delimiters.setup({})
 
